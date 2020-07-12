37c8a66504cc771c3caf788647e8297c195f108c
kennedy joseph <jken04680@gmail.com>
2020-07-12 01:29:10 +0100

    second commit

--- lib/data.dart
+++ lib/data.dart
@@ -1,12 +1,126 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Category{
-  final String name;
+  final String rate;
+  final String food;
   final Icon icon;
+  final String fName;
+  final String money;
 
 
   Category({
-   @required this.name,
+   @required this.rate,
+   @required this.food,
    @required this.icon,
+   @required this.fName,
+   @required this.money,
   });
+
+  List<Category> createSampleList() {
+    List _category = List<Category>();
+    return _category
+    ..add(Category(
+      rate: '4.8',
+      food: 'assets/image/food.png',
+      icon: Icon(Icons.star),
+      fName: 'Breakfast',
+      money: '5.00'
+    )) 
+     ..add(Category(
+      rate: '4.8',
+      food: 'assets/image/food.png',
+      icon: Icon(Icons.star),
+      fName: 'Breakfast',
+      money: '5.00'
+    ))
+     ..add(Category(
+      rate: '4.8',
+      food: 'assets/image/food.png',
+      icon: Icon(Icons.star),
+      fName: 'Breakfast',
+      money: '5.00'
+    ))
+     ..add(Category(
+      rate: '4.8',
+      food: 'assets/image/food.png',
+      icon: Icon(Icons.star),
+      fName: 'Breakfast',
+      money: '5.00'
+    ))
+     ..add(Category(
+      rate: '4.8',
+      food: 'assets/image/food.png',
+      icon: Icon(Icons.star),
+      fName: 'Breakfast',
+      money: '5.00'
+    ))
+     ..add(Category(
+      rate: '4.8',
+      food: 'assets/image/food.png',
+      icon: Icon(Icons.star),
+      fName: 'Breakfast',
+      money: '5.00'
+    ));  
+  }
+}
+
+class Name extends StatelessWidget {
+  const Name({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView(
+                          children: <Widget>[
+                            Container(
+                              height: 200,
+                              width: 200,
+                              child: GridView.count(
+                                crossAxisCount: 2,
+                                primary: false,
+                                crossAxisSpacing: 10.0,
+                                mainAxisSpacing: 15.0,
+                                //childAspectRatio: 0.8,
+                                children: <Widget>[
+                                  Padding(
+                                    padding: EdgeInsets.only(top: 3, left: 5, right: 5, bottom: 5),
+                                    child: InkWell(
+                                      onTap: (){},
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(5),
+                                          boxShadow: [
+                                            BoxShadow(
+                                              color: Colors.grey.withOpacity(0.2),
+                                              spreadRadius: 2.0,
+                                              blurRadius: 3.0,
+                                            )
+                                          ],
+                                          color: Colors.white,
+                                        ),
+                                        child: Column(
+                                          children: <Widget>[
+                                            Container( 
+                                            child: Row(
+                                              crossAxisAlignment: CrossAxisAlignment.center,
+                                              mainAxisAlignment: MainAxisAlignment.center,
+                                              children: <Widget>[
+                                                Text('4.5', style: TextStyle(color: Color(0xff707070), fontSize: 15),),
+                                                Icon(Icons.star, color: Colors.yellow)
+                                              ],
+                                            ),
+                                            ),
+                                            //Hero(
+                                              
+                                            //)
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+    );
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//import 'package:ui_meals/try.dart';
 import './data.dart';
 
 void main() => runApp(MyApp());
@@ -26,113 +27,293 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  final List<Category> category = [
-    Category(
-      icon: Icon(Icons.fastfood, size: 20),
-      name: 'All',
-    ),
-    Category(
-      icon: Icon(Icons.donut_large, size: 20),
-      name: 'Vegas',
-    ),
-    Category(
-      icon: Icon(Icons.donut_small, size: 20),
-      name: 'Snacks',
-    ),
-    Category(
-      icon: Icon(Icons.local_drink, size: 20),
-      name: 'Drink',
-    ),
-    Category(
-      icon: Icon(Icons.folder_open, size: 20),
-      name: 'Donuts',
-    ),
-  ];
-   
+  List _category = List<Category>();
+  double _screenWidthAdjustment;
+
+  @override
+  void initState() { 
+    super.initState();
+    _category = Category().createSampleList();
+  }
+
+  @override
+  void didChangeDependencies(){
+    super.didChangeDependencies();
+    _screenWidthAdjustment = MediaQuery.of(context).size.width - 48.0 - 64.0;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-         padding: EdgeInsets.only(top:40, left: 20, right: 20, bottom: 10),
-        child: Column(
-          children: <Widget>[
-            Container(
-              //padding: EdgeInsets.only(top:40, left: 20, right: 20, bottom: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Icon(Icons.arrow_back_ios, size: 30,),
-                  Icon(Icons.search, size: 30),
-                ],
-              ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height/1 - 70,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(50), bottomRight: Radius.circular(50)),
             ),
-            SizedBox(height: 20),
-            Container(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Text('Order', style: TextStyle(fontSize: 30)),
-                  SizedBox(height: 10,),
-
-                  Container(
-                    height: MediaQuery.of(context).size.height/2 -200,
-                    //color: Colors.blue,
-                    child: SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: category.map((tx) {
-                          return Padding(
-                            padding: const EdgeInsets.only(right:10),
-                            child: Container(
-                              height: 250,
-                              width: 85,
-                              //padding: EdgeInsets.only(left: 3, right: 10),
-                              //color: Colors.red,
-                              decoration: BoxDecoration(
-                                color: Color(0xffffffff),
-                                borderRadius: BorderRadius.circular(40),
-                                border: Border.all(width: 2, color: Color(0xffBCBABA))
-                              ),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: <Widget>[
-                                  Padding(
-                                    padding: const EdgeInsets.only(top: 20.0),
-                                    child: Container(
-                                      height: 50,
-                                      width: 50,
-                                      decoration: BoxDecoration(
-                                        color: Color(0xffeaeaea),
-                                        borderRadius: BorderRadius.circular(50),
-                                        border: Border.all(width: 2, color: Color(0xff747474))
-                                      ),
-                                      child: tx.icon,
-                                    ),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top:35, left: 15, right: 15, bottom: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Icon(Icons.arrow_back_ios, size: 30,),
+                      Icon(Icons.search, size: 30),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 10,),
+                Container(
+                  padding: EdgeInsets.only(left: 15, right: 15, bottom: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Text('Order', style: TextStyle(fontSize: 30)),
+                      SizedBox(height: 10,),
+                      Container(
+                        height: MediaQuery.of(context).size.height/2 -220,
+                        color: Colors.blueGrey,
+                        child: SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              InkWell(
+                                onTap: (){},
+                                child: Padding(
+                                padding: const EdgeInsets.only(right:10),
+                                child: Container(
+                                  height: 200,
+                                  width: 85,
+                                  //padding: EdgeInsets.only(left: 3, right: 10),
+                                  //color: Colors.red,
+                                  decoration: BoxDecoration(
+                                    color: Color(0xffffffff),
+                                    borderRadius: BorderRadius.circular(40),
+                                    border: Border.all(width: 2, color: Color(0xffBCBABA))
                                   ),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: <Widget>[
+                                      Padding(
+                                        padding: const EdgeInsets.only(top: 20.0),
+                                        child: Container(
+                                          height: 50,
+                                          width: 50,
+                                          decoration: BoxDecoration(
+                                            color: Color(0xffeaeaea),
+                                            borderRadius: BorderRadius.circular(50),
+                                            border: Border.all(width: 2, color: Color(0xff747474))
+                                          ),
+                                          child: Icon(Icons.fastfood),
+                                        ),
+                                      ),
 
-                                  SizedBox(height: 20,),
-                                  Text(
-                                    tx.name,
-                                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-                                  )
-                                ],
+                                      SizedBox(height: 10,),
+                                      Text(
+                                        'All',
+                                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                                      )
+                                    ],
+                                  ),
+                                ),
                               ),
-                            ),
-                          );
-                        }).toList(),
-                      ),
-                    ),
-                  )
+                              ),
+                              InkWell(
+                                onTap: (){},
+                                child: Padding(
+                                padding: const EdgeInsets.only(right:10),
+                                child: Container(
+                                  height: 200,
+                                  width: 85,
+                                  //padding: EdgeInsets.only(left: 3, right: 10),
+                                  //color: Colors.red,
+                                  decoration: BoxDecoration(
+                                    color: Color(0xffffffff),
+                                    borderRadius: BorderRadius.circular(40),
+                                    border: Border.all(width: 2, color: Color(0xffBCBABA))
+                                  ),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: <Widget>[
+                                      Padding(
+                                        padding: const EdgeInsets.only(top: 20.0),
+                                        child: Container(
+                                          height: 50,
+                                          width: 50,
+                                          decoration: BoxDecoration(
+                                            color: Color(0xffeaeaea),
+                                            borderRadius: BorderRadius.circular(50),
+                                            border: Border.all(width: 2, color: Color(0xff747474))
+                                          ),
+                                          child: Icon(Icons.donut_large),
+                                        ),
+                                      ),
 
-                ],
-              ),
-            )
-          ],
-        ),
+                                      SizedBox(height: 10,),
+                                      Text(
+                                        'All',
+                                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              ),
+                              InkWell(
+                                onTap: (){},
+                                child: Padding(
+                                padding: const EdgeInsets.only(right:10),
+                                child: Container(
+                                  height: 200,
+                                  width: 85,
+                                  //padding: EdgeInsets.only(left: 3, right: 10),
+                                  //color: Colors.red,
+                                  decoration: BoxDecoration(
+                                    color: Color(0xffffffff),
+                                    borderRadius: BorderRadius.circular(40),
+                                    border: Border.all(width: 2, color: Color(0xffBCBABA))
+                                  ),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: <Widget>[
+                                      Padding(
+                                        padding: const EdgeInsets.only(top: 20.0),
+                                        child: Container(
+                                          height: 50,
+                                          width: 50,
+                                          decoration: BoxDecoration(
+                                            color: Color(0xffeaeaea),
+                                            borderRadius: BorderRadius.circular(50),
+                                            border: Border.all(width: 2, color: Color(0xff747474))
+                                          ),
+                                          child: Icon(Icons.donut_small),
+                                        ),
+                                      ),
+
+                                      SizedBox(height: 10,),
+                                      Text(
+                                        'All',
+                                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              ),
+                              InkWell(
+                                onTap: (){},
+                                child: Padding(
+                                padding: const EdgeInsets.only(right:10),
+                                child: Container(
+                                  height: 200,
+                                  width: 85,
+                                  //padding: EdgeInsets.only(left: 3, right: 10),
+                                  //color: Colors.red,
+                                  decoration: BoxDecoration(
+                                    color: Color(0xffffffff),
+                                    borderRadius: BorderRadius.circular(40),
+                                    border: Border.all(width: 2, color: Color(0xffBCBABA))
+                                  ),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: <Widget>[
+                                      Padding(
+                                        padding: const EdgeInsets.only(top: 20.0),
+                                        child: Container(
+                                          height: 50,
+                                          width: 50,
+                                          decoration: BoxDecoration(
+                                            color: Color(0xffeaeaea),
+                                            borderRadius: BorderRadius.circular(50),
+                                            border: Border.all(width: 2, color: Color(0xff747474))
+                                          ),
+                                          child: Icon(Icons.local_bar),
+                                        ),
+                                      ),
+
+                                      SizedBox(height: 10,),
+                                      Text(
+                                        'All',
+                                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              ),
+                              InkWell(
+                                onTap: (){},
+                                child: Padding(
+                                padding: const EdgeInsets.only(right:10),
+                                child: Container(
+                                  height: 200,
+                                  width: 85,
+                                  //padding: EdgeInsets.only(left: 3, right: 10),
+                                  //color: Colors.red,
+                                  decoration: BoxDecoration(
+                                    color: Color(0xffffffff),
+                                    borderRadius: BorderRadius.circular(40),
+                                    border: Border.all(width: 2, color: Color(0xffBCBABA))
+                                  ),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: <Widget>[
+                                      Padding(
+                                        padding: const EdgeInsets.only(top: 20.0),
+                                        child: Container(
+                                          height: 50,
+                                          width: 50,
+                                          decoration: BoxDecoration(
+                                            color: Color(0xffeaeaea),
+                                            borderRadius: BorderRadius.circular(50),
+                                            border: Border.all(width: 2, color: Color(0xff747474))
+                                          ),
+                                          child: Icon(Icons.home),
+                                        ),
+                                      ),
+
+                                      SizedBox(height: 10,),
+                                      Text(
+                                        'All',
+                                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              ), 
+                            ],
+                          )
+                        ),
+                      ),
+                      SizedBox(height: 3,),
+                      Container(
+                        height: 344,
+                        width: double.infinity,
+                        //color: Colors.redAccent,
+                        child: ,
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(),
+        ],
       ),
     );
   }
+  
 }
